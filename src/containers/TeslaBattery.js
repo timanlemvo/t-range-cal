0359aab2a078442f4483aacbea29d6424e641b65
timanlemvo <timanlemvo@gmail.com>
2021-10-12 16:01:40 -0700

    created and updated containers

---
+++ src/containers/TeslaBattery.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TeslaBattery.css';
+
+class TeslaBattery extends React.Component {
+    render() {
+        return (
+            <form className="tesla-battery">
+                <h1>Range Per Charge</h1>
+            </form>
+        )
+    }
+}
+
+export default TeslaBattery;
+    
