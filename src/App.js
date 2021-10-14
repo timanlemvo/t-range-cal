ff09de4ad61d04449446f50332279cfa9b9478ab
timanlemvo <timanlemvo@gmail.com>
2021-10-14 13:34:47 -0700

    updated TeslaCar globally

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React,  { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import TeslaBattery from './containers/TeslaBattery';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <TeslaBattery />
       </div>
     );
   }
 }
-
 export default App;
--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './TeslaBattery.css';
 import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
+import TeslaCar from '../components/TeslaCar/TeslaCar';
 
 
 class TeslaBattery extends React.Component {
@@ -8,6 +9,7 @@ class TeslaBattery extends React.Component {
         return (
             <form className="tesla-battery">
                 <h1>Range Per Charge</h1>
+                <TeslaCar />
                 <TeslaNotice />
             </form>
         )
