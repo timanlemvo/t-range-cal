ce02bf9fee506743c8292668cfc0bd9bef5c00a8
timanlemvo <timanlemvo@gmail.com>
2021-10-19 14:22:02 -0700

    removed unnecessary props

--- src/components/TeslaClimate/TeslaClimate.js
+++ src/components/TeslaClimate/TeslaClimate.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import './TeslaClimate.css';
 
-const TeslaClimate = (props) => (
-  <div className="tesla-climate">
-    <label
-      className={`tesla-climate__item ${props.value ? 'tesla-climate__item--active' : '' }  ${!props.limit ? 'tesla-heat':''}`}
-    >
-      <p>{props.limit ? 'ac' : 'heat'} {props.value ? 'on' : 'off'}</p>
-      <i className="tesla-climate__icon"></i>
-      <input
-        type="checkbox"
-        name="climate"
-        checked={props.value}
-        onChange={() => {props.handleChangeClimate()}}
-      />
-    </label>
-  </div>
-);
+function TeslaClimate(props) {
+    return (
+        <div className='tesla-climate'>
+            <label className={`tesla-climate__item ${props.value ? 'tesla-climate__item--active' : ''} ${!props.limit ? 'tesla-heat' : ''}`}>
+                <p>{props.limit ? 'ac' : 'heat'} {props.value ? 'on' : 'off'}</p>
+                <i className='tesla-climate__icon'></i>
+                <input
+                    type='checkbox'
+                    name='climate'
+                    checked={props.value}
+                    onChange={() => { props.handleChangeClimate() }}
+                />
+            </label>
 
-TeslaClimate.propTypes = {
-  value: React.PropTypes.bool,
-  limit: React.PropTypes.bool,
-  handleChangeClimate: React.PropTypes.func
+        </div>
+    )
 }
 
 export default TeslaClimate;
--- src/components/TeslaCounter/TeslaCounter.js
+++ src/components/TeslaCounter/TeslaCounter.js
@@ -1,41 +1,76 @@
 import React from 'react';
 import './TeslaCounter.css';
 
-const TeslaCounter = ({ initValues, currentValue, increment, decrement }) => (
-  <div className="tesla-counter">
-    <p className="tesla-counter__title">{initValues.title}</p>
-    <div className="tesla-counter__container cf">
-      <div className="tesla-counter__item">
-        <p className="tesla-counter__number">
-          { currentValue }
-          <span>{ initValues.unit }</span>
-        </p>
-        <div className="tesla-counter__controls">
-          <button 
-            onClick={(e) => {
-              e.preventDefault();
-              increment(currentValue)}} 
-            disabled={currentValue >= initValues.max} 
-          >
-          </button>
-          <button 
-            onClick={(e) => {
-              e.preventDefault();
-              decrement(currentValue)}} 
-            disabled={currentValue <= initValues.min} 
-          >
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>  
-);
+class TeslaCounter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            animationEffect: false,
+            direction: ''
+        }
+    }
 
-TeslaCounter.propTypes = {
-  currentValue: React.PropTypes.number,
-  increment: React.PropTypes.func,
-  decrement: React.PropTypes.func,
-  initValues: React.PropTypes.object
-}
+    componentWillReceiveProps(nextProps) {
+        // const { currentValue, initValues } = nextProps;
+        if (this.props.currentValue !== nextProps.currentValue) {
+            const direction = this.props.currentValue < nextProps.currentValue ? 'increase' : 'decrese';
+            this.setState({
+                animationEffect: true,
+                direction
+            });
+
+            setTimeout(
+                () => {
+                    this.setState({
+                        animationEffect: false
+                    })
+                }, 500
+            );
+            return;
+        }
+    }
+
+    render() {
+        const { initValues, currentValue, increment, decrement } = this.props;
+        const { animationEffect, direction } = this.state;
+        const animationClass = animationEffect
+            ? (direction === 'increase' ? 'flip-in-hor-top' : 'flip-in-hor-bottom')
+            : '';
+
+        return (
+            <div className="tesla-counter">
+                <p className="tesla-counter__title">{initValues.title}</p>
+                <div className="tesla-counter__container cf">
+                    <div className="tesla-counter__item">
+                        <div className={`${animationClass}`}>
+                            <p className="tesla-counter__number ">
+                                {currentValue}
+                                <span>{initValues.unit}</span>
+                            </p>
+                        </div>
+                        <div className="tesla-counter__controls">
+                            <button
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    increment(currentValue)
+                                }}
+                                disabled={currentValue >= initValues.max}
+                            >
+                            </button>
+                            <button
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    decrement(currentValue)
+                                }}
+                                disabled={currentValue <= initValues.min}
+                            >
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+};
 
 export default TeslaCounter;
